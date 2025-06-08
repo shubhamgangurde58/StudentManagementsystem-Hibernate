35180bc2889c9f3afe8c4eed75604f30f41787b1
shubhamgangurde58 <shubhamgangurde58@gmail.com>
2025-06-08 13:06:32 +0530

    [ADDED]: shubham This code for Student Management System Project

---
+++ StudentManagementSystem/src/main/java/com/shubham/StudentManagementSystem/App.java
@@ -0,0 +1,10 @@
+package com.shubham.StudentManagementSystem;
+
+public class App {
+	
+    public static void main(String[] args) {
+
+    	System.out.println("!!! Welcom To Student Management System !!!");
+
+    }
+}
